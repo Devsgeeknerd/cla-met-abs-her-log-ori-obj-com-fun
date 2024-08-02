691d55942ade9d32e0d04f6ebb7b5784ef14abf1
Paulo Vanderley <vanderley.1109@gmail.com>
2024-08-02 16:07:49 -0300

    :page_with_curl: Update file content.

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package br.com.devsgeeknerd.zoologico.classes;
+
+public abstract class Animal {
+  private String nome;
+  private String especie;
+  private int idade;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
