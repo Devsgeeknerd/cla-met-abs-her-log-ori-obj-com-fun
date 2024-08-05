0ae3bbab1e249aa63723e193ccd96cc7c3cde148
Paulo Vanderley <vanderley.1109@gmail.com>
2024-08-05 16:19:31 -0300

    :page_with_curl: Update file content.

--- Cachorro.java
+++ Cachorro.java
@@ -4,4 +4,8 @@ public class Cachorro extends Animal {
     public Cachorro(String nome) {
         super(nome);
     }
+
+    public Cachorro(String nome, String especie, int idade) {
+        super(nome, idade, especie);
+    }
 }
