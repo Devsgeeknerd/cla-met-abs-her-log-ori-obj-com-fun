8845f4b179a56d6b528c326e0d07aa223cd74577
Paulo Vanderley <vanderley.1109@gmail.com>
2024-08-05 16:42:59 -0300

    :page_with_curl: Update file content.

--- programa.java
+++ programa.java
@@ -7,5 +7,6 @@ import br.com.devsgeeknerd.zoologico.classes.Gato;
 public class Programa {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro("Toto");
+        Gato gato = new Gato("Tata", 2);
     }
 }
