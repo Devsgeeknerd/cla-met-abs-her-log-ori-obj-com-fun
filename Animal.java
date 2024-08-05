73a27420c830918e117db9a9448860dc1eb6f9d9
Paulo Vanderley <vanderley.1109@gmail.com>
2024-08-05 16:16:38 -0300

    :page_with_curl: Update file content.

--- Animal.java
+++ Animal.java
@@ -1,49 +1,49 @@
 package br.com.devsgeeknerd.zoologico.classes;
 
 public abstract class Animal {
-  private String nome;
-  private String especie;
-  private int idade;
+    private String nome;
+    private String especie;
+    private int idade;
 
-  public String getNome() {
-    return nome;
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-  public String getEspecie() {
-    return especie;
-  }
+    public String getEspecie() {
+        return especie;
+    }
 
-  public void setEspecie(String especie) {
-    this.especie = especie;
-  }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
 
-  public int getIdade() {
-    return idade;
-  }
+    public int getIdade() {
+        return idade;
+    }
 
-  public void setIdade(int idade) {
-    this.idade = idade;
-  }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
-  public Animal(String nome, int idade, String especie) {
-    this.nome = nome;
-    this.especie = especie;
-    this.idade = idade;
-  }
+    public Animal(String nome, int idade, String especie) {
+        this.nome = nome;
+        this.especie = especie;
+        this.idade = idade;
+    }
 
-  // Sobrecarga
-  public Animal(String nome) {
-    this.nome = nome;
-  }
+    // Sobrecarga
+    public Animal(String nome) {
+        this.nome = nome;
+    }
 
-  public abstract void emitirBarulho();
+    public abstract void emitirBarulho();
 
-  public boolean ehAdulto() {
-    return idade >= 1;
-  }
+    public boolean ehAdulto() {
+        return idade >= 1;
+    }
 
 }
